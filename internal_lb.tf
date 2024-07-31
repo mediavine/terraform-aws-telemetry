56d02aca4b952c28aa16287692c576299821a47a
Keith Miller <36780654+Keimille@users.noreply.github.com>
2024-07-31 11:45:47 -0400

    Adds otel collector contrib service (#19)
    * Adds otel-collector-contrib options refrences Add otel-collector-contrib service #17
    
    * remove duplicate resource names for ecs services
    
    * Addresses Update autoscaling options for collector service #16 Addresses Container Counts (desired count and scaling options) should be changed from hard-coded to dynamic #15
    
    * addresses Container Counts (desired count and scaling options) should be changed from hard-coded to dynamic #15 and Update autoscaling options for collector service #16
    
    * Update date validation logic for one ecs service at a time
    
    * fixing some validate errors
    
    * Fixing count logic in lb and security group resources
    
    * use secrets instead of environment
    
    * fix bug on command string
    
    * update terraform docs

--- internal_lb.tf
+++ internal_lb.tf
@@ -3,7 +3,7 @@ data "aws_vpc" "this" {
 }
 
 resource "aws_lb" "this" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   name               = "${var.name}-internal-collector-lb"
   internal           = true
@@ -16,21 +16,21 @@ resource "aws_lb" "this" {
 # LB Listener Rules
 ################################################################################
 
-resource "aws_lb_listener" "health_check" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+# resource "aws_lb_listener" "health_check" {
+#   count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
 
-  load_balancer_arn = aws_lb.this[0].arn
-  port              = 13133
-  protocol          = "HTTP"
+#   load_balancer_arn = aws_lb.this[0].arn
+#   port              = 13133
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.health_check[0].arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.health_check[0].arn
+#   }
+# }
 
 resource "aws_lb_listener" "http" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   load_balancer_arn = aws_lb.this[0].arn
   port              = 4318
@@ -46,30 +46,30 @@ resource "aws_lb_listener" "http" {
 # LB Target Groups
 ################################################################################
 
-resource "aws_lb_target_group" "health_check" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+# resource "aws_lb_target_group" "health_check" {
+#   count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
 
-  name        = "${var.name}-internal-collector-tg"
-  port        = 13133
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = var.vpc_id
+#   name        = "${var.name}-internal-collector-tg"
+#   port        = 13133
+#   protocol    = "HTTP"
+#   target_type = "ip"
+#   vpc_id      = var.vpc_id
 
-  health_check {
-    path                = "/healthcheck"
-    protocol            = "HTTP"
-    interval            = 30
-    timeout             = 10
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    matcher             = "200-399"
-  }
+#   health_check {
+#     path                = "/healthcheck"
+#     protocol            = "HTTP"
+#     interval            = 30
+#     timeout             = 10
+#     healthy_threshold   = 3
+#     unhealthy_threshold = 3
+#     matcher             = "200-399"
+#   }
 
-  depends_on = [aws_lb.this]
-}
+#   depends_on = [aws_lb.this]
+# }
 
 resource "aws_lb_target_group" "http" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   name        = "${var.name}-collector-http-tg"
   port        = 4318
@@ -79,7 +79,8 @@ resource "aws_lb_target_group" "http" {
 
   health_check {
     matcher = "200-499"
-    path    = "/v1/metrics"
+    path    = "/healthcheck"
+    port    = 13133
   }
 
   depends_on = [aws_lb.this]
@@ -89,7 +90,7 @@ resource "aws_lb_target_group" "http" {
 # Security Group & Rules
 ################################################################################
 resource "aws_security_group" "this" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   name        = "${var.name}-internal-collector-sg"
   description = "Security group for the internal collector load balancer"
@@ -97,7 +98,7 @@ resource "aws_security_group" "this" {
 }
 
 resource "aws_security_group_rule" "health_check" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   type              = "ingress"
   from_port         = 13133
@@ -108,7 +109,7 @@ resource "aws_security_group_rule" "health_check" {
 }
 
 resource "aws_security_group_rule" "http" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   type              = "ingress"
   from_port         = 4318
@@ -119,7 +120,7 @@ resource "aws_security_group_rule" "http" {
 }
 
 resource "aws_security_group_rule" "egress" {
-  count = var.enable_internal_lb && var.create_adot_service ? 1 : 0
+  count = var.enable_internal_lb && var.create_adot_service || var.enable_internal_lb && var.create_otel_collector_service ? 1 : 0
 
   type              = "egress"
   from_port         = 0
---
+++ otel_collector_contrib_ecs.tf
@@ -0,0 +1,107 @@
+locals {
+  default_config_environment = [
+    {
+      name  = "AWS_REGION"
+      value = var.region
+    }
+  ]
+
+  custom_config_environment = [
+
+    {
+      name      = "CUSTOM_OTEL_CONFIG"
+      valueFrom = "${aws_ssm_parameter.custom_otel_config[0].arn}"
+    }
+  ]
+
+  environment = var.custom_otel_config[0].otel_config_file_path != null ? local.custom_config_environment : local.default_config_environment
+}
+
+resource "random_string" "this" {
+  length  = 7
+  special = false
+  upper   = true
+  lower   = false
+}
+
+resource "aws_ssm_parameter" "custom_otel_config" {
+  count = var.custom_otel_config != null ? 1 : 0
+
+  name  = "CUSTOM_OTEL_CONFIG_${random_string.this.result}"
+  type  = "String"
+  value = file(var.custom_otel_config[0].otel_config_file_path)
+}
+
+resource "aws_ecs_service" "otel_collector_ecs_service" {
+  count = var.create_otel_collector_service ? 1 : 0
+
+  name            = "${var.name}-otel-collector"
+  cluster         = var.cluster
+  task_definition = aws_ecs_task_definition.otel_collector_task_definition[0].arn
+  launch_type     = "FARGATE"
+  desired_count   = var.desired_count
+  network_configuration {
+    subnets          = var.subnets
+    security_groups  = [aws_security_group.this[0].id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.http[0].arn
+    container_name   = "otel-collector"
+    container_port   = 4318
+  }
+}
+
+resource "aws_cloudwatch_log_group" "otel_collector_log_group" {
+  count = var.create_otel_collector_service ? 1 : 0
+
+  name              = "/ecs/${var.name}-otel-collector"
+  retention_in_days = 7
+}
+
+resource "aws_ecs_task_definition" "otel_collector_task_definition" {
+  count = var.create_otel_collector_service ? 1 : 0
+
+  family                   = "${var.name}-otel-collector-contrib"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.otel_collector_cpu
+  memory                   = var.otel_collector_memory
+  execution_role_arn       = aws_iam_role.execution_role[0].arn
+  task_role_arn            = aws_iam_role.task_role[0].arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "otel-collector"
+      image     = var.otel_collector_image
+      cpu       = var.otel_collector_cpu
+      memory    = var.otel_collector_memory
+      essential = true
+      command   = var.custom_otel_config != null ? ["--config=env:CUSTOM_OTEL_CONFIG"] : []
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.otel_collector_log_group[0].name
+          "awslogs-region"        = var.region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+      portMappings = [
+        {
+          containerPort = 4318
+          hostPort      = 4318
+        },
+        {
+          containerPort = 4317
+          hostPort      = 4317
+        },
+        {
+          containerPort = 13133
+          hostPort      = 13133
+        },
+      ]
+      secrets = local.environment
+    }
+  ])
+}
